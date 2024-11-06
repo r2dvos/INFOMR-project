d486aa4252f352949c3d6a347433be141abf22c5
Foca Bogdan <81586357+Zeyt8@users.noreply.github.com>
2024-11-06 01:33:38 +0100

    Update roc.py

--- roc.py
+++ roc.py
@@ -1,50 +1,22 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def plot_sensitivity_vs_specificity(sensitivity, specificity, class_label):
+def plot_sensitivity_vs_specificity(sensitivity_knn, specificity_knn, sensitivity_normal, specificity_normal, class_label):
     plt.figure(figsize=(10, 6))
-    plt.plot(sensitivity, specificity)
+    plt.plot(sensitivity_knn, specificity_knn)
+    plt.plot(sensitivity_normal, specificity_normal)
     plt.xlabel('Sensitivity')
     plt.ylabel('Specificity')
     plt.title(f'ROC for {class_label}')
     plt.xlim(0, 1)
     plt.ylim(0, 1)
     plt.grid(True)
+    plt.legend(['KNN', 'Our Method'])
     plt.show()
 
-if __name__ == "__main__":
-    df = pd.read_csv('database.csv')
-    class_counts = df['Class'].value_counts().to_dict()
-    d = df.shape[0]
-
-    data = pd.read_csv("results_knn_tau.txt", sep=';', header=None)
-    data.columns = ['Class'] + [f'Tau_{i+1}' for i in range(data.shape[1] - 1)]
-
+def calculate_sensitivity_specificity(data, class_counts, d):
     sensitivities = []
     specificities = []
-    """
-    for index, row in data.iterrows():
-        cls = row['Class']
-        tp = row[f'Tau_{tau}']
-        c = class_counts[cls]
-        tn = d - c - (tau - tp)
-        sensitivity = tp / c
-        specificity = tn / (d - c)
-        sensitivities.append(sensitivity)
-        specificities.append(specificity)
-    """
-    """
-    for tau in range(1, 2485):
-        cls = data.iloc[0]['Class']
-        tp = data.iloc[0][f'Tau_{tau}']
-        c = class_counts[cls]
-        tn = d - c - (tau - tp)
-        sensitivity = tp / c
-        specificity = tn / (d - c)
-        sensitivities.append(sensitivity)
-        specificities.append(specificity)
-    """
-
     for tau in range(1, 2485):
         sensitivity = 0.0
         specificity = 0.0
@@ -60,31 +32,20 @@ if __name__ == "__main__":
         sensitivities.append(sensitivity/count)
         specificities.append(specificity/count)
         print(f'calculated tau {tau}')
+    return sensitivities, specificities
 
-    print(sensitivities)
-    print("\n~~\n")
-    print(specificities)
+if __name__ == "__main__":
+    df = pd.read_csv('database.csv')
+    class_counts = df['Class'].value_counts().to_dict()
+    d = df.shape[0]
 
-    plot_sensitivity_vs_specificity(sensitivities, specificities, "Entire DB")
+    data_knn = pd.read_csv("results_knn_tau.txt", sep=';', header=None)
+    data_knn.columns = ['Class'] + [f'Tau_{i+1}' for i in range(data_knn.shape[1] - 1)]
 
-    """
-    class_sensitivity_specificity = {}
-    for tau in range(1, 101):
-        for index, row in data.iterrows():
-            cls = row['Class']
-            tp = row[f'Tau_{tau}']
-            c = class_counts[cls]
-            tn = d - c - (tau - tp)
-            sensitivity = tp / c
-            specificity = tn / (d - c)
-            if cls not in class_sensitivity_specificity:
-                class_sensitivity_specificity[cls] = {'sensitivity': [0]*100, 'specificity': [0]*100, 'count': [0]*100}
-            class_sensitivity_specificity[cls]['sensitivity'][tau-1] += sensitivity
-            class_sensitivity_specificity[cls]['specificity'][tau-1] += specificity
-            class_sensitivity_specificity[cls]['count'][tau-1] += 1
+    data_normal = pd.read_csv("results_normal_tau.txt", sep=';', header=None)
+    data_normal.columns = ['Class'] + [f'Tau_{i+1}' for i in range(data_normal.shape[1] - 1)]
 
-    for cls, values in class_sensitivity_specificity.items():
-        avg_sensitivity = [values['sensitivity'][i] / values['count'][i] if values['count'][i] != 0 else 0 for i in range(100)]
-        avg_specificity = [values['specificity'][i] / values['count'][i] if values['count'][i] != 0 else 0 for i in range(100)]
-        plot_sensitivity_vs_specificity(avg_sensitivity, avg_specificity, cls)
-    """
+    sensitivities_knn, specificities_knn = calculate_sensitivity_specificity(data_knn, class_counts, d)
+    sensitivities_normal, specificities_normal = calculate_sensitivity_specificity(data_normal, class_counts, d)
+
+    plot_sensitivity_vs_specificity(sensitivities_knn, specificities_knn, sensitivities_normal, specificities_normal, "Entire DB")
